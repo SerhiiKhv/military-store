dfbcff5b13445efe045aa35ee2c162eeacb4dc83
Alamay <khvechuksergiyko@gmail.com>
2024-04-10 19:33:53 +0300

    Fix

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,11 +1,12 @@
-import NextAuth from "next-auth";
-import CredentialsProvider from "next-auth/providers/credentials";
-import mongoose from "mongoose";
-import bcrypt from "bcrypt";
-import { User } from "@/app/models/User";
-import GoogleProvider from "next-auth/providers/google";
+import { NextApiRequest, NextApiResponse } from 'next';
+import NextAuth from 'next-auth';
+import CredentialsProvider from 'next-auth/providers/credentials';
+import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
+import { User } from '@/app/models/User';
+import GoogleProvider from 'next-auth/providers/google';
 
-export const authOptions = {
+const authOptions = {
     secret: process.env.SECRET,
     providers: [
         GoogleProvider({
@@ -13,35 +14,35 @@ export const authOptions = {
             clientSecret: String(process.env.GOOGLE_CLIENT_SECRET),
         }),
         CredentialsProvider({
-            type: "credentials",
-            name: "Credentials",
-            id: "credentials",
+            type: 'credentials',
+            name: 'Credentials',
+            id: 'credentials',
             credentials: {
-                email: { label: "Email", type: "email", placeholder: "test@gmail.com" },
-                password: { label: "Password", type: "password" },
-                name: { label: "UserName", type: "text" },
+                email: { label: 'Email', type: 'email', placeholder: 'test@gmail.com' },
+                password: { label: 'Password', type: 'password' },
+                name: { label: 'UserName', type: 'text' },
             },
             async authorize(credentials, req) {
                 await mongoose.connect(String(process.env.MONGO_URL));
 
-                try{
+                try {
                     const email = credentials?.email;
                     const password = credentials?.password;
 
                     const user = await User.findOne({ email });
 
-                    if(user) {
+                    if (user) {
                         const passwordOk = bcrypt.compareSync(String(password), user.password);
                         if (passwordOk) {
                             return user;
                         }
                     }
-                }catch(err: any){
-                    throw new Error(err)
+                } catch (err: any) {
+                    throw new Error(err);
                 }
             },
         }),
-    ]
+    ],
 };
 
-export default NextAuth(authOptions);
+export default (req: NextApiRequest, res: NextApiResponse) => NextAuth(req, res, authOptions);
