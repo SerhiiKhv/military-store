c7338f288ae444663301100d93e7a3319a14636b
Alamay <khvechuksergiyko@gmail.com>
2024-04-16 13:25:34 +0300

    Fix (Deploy)

--- src/components/screenSize/screenSize.ts
+++
@@ -1,6 +0,0 @@
-import {useMediaQuery} from "@react-hook/media-query";
-
-const isSmallScreen = useMediaQuery('(max-width: 640px)');
-const isMediumScreen = useMediaQuery('(min-width: 641px) and (max-width: 1024px)');
-const isLargeScreen = useMediaQuery('(min-width: 1025px)');
-
